c43d9bfa21d90a2e95f726374e4b084e077bb905
Dorayaki-Chan <zero.kotam@gmail.com>
2022-05-20 20:43:05 +0900

    画像一つを読み込む

---
+++ processPic/3kyokuka_kai.py
@@ -0,0 +1,73 @@
+import cv2
+
+class ProcessingPic:
+    def __init__(self, fle):
+        # 元画像読み込み
+        img=cv2.imread(fle,0)
+        # 元画像を表示
+        # cv2.imshow("original",img)
+
+        color_area = self.threeColor(img)
+
+        self.black_area = color_area['black_area']
+        self.white_area = color_area['white_area']
+        self.gray_area = color_area['gray_area']
+    
+    def threeColor(self, img):
+        # 画像の縦の画素数、横の画素数を取得
+        height,width=img.shape
+
+        # 黒にする上限を指定
+        th_low_value=25
+        # 白にする上限を指定
+        th_high_value=150
+        # 灰色を定義
+        gray=100
+        # 元画像全体の画素数
+        whole_area=img.size
+
+        # 三色の面積初期化
+        white_area=0
+        black_area=0
+        gray_area=0
+
+
+        # 画像の３値化と各色の面積比を求める
+        for i in range(height):
+            for j in range(width):
+                if img[i,j]<th_low_value:
+                    img[i,j]=0
+                    black_area+=1
+                elif th_low_value <= img[i,j] <=th_high_value:
+                    img[i,j]=gray
+                    gray_area+=1
+                else :
+                    img[i,j]=255
+                    white_area+=1
+
+        # 3値化した画像の表示
+        # cv2.imshow("create",img)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
+        dic = {
+            'black_area':black_area/whole_area*100, 
+            'white_area':white_area/whole_area*100, 
+            'gray_area':gray_area/whole_area*100
+        }
+
+        return dic
+
+
+def main():
+    fle = './samplePics/space_1.jpg'
+    x = ProcessingPic(fle)
+    print(round(x.black_area, 2))
+
+    # if pics[dic_name].black_area > 80:
+    #     print("ひっくり返ってます")
+    # else:
+    #     print("正常です")
+
+if __name__ == '__main__':
+    main()
